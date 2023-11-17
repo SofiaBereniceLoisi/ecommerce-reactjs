6955fcfa47b16f8c7917312ada5731f8d1b99dda
Sofía Loisi <sofiaberenice.loisi@gmail.com>
2023-11-16 21:56:27 -0300

    relleno de aboutPage

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,8 +1,20 @@
-
+import "./aboutPage.css";
+import { useNavigate } from "react-router-dom";
 
 const AboutPage = () => {
+    const navigate = useNavigate();
+
     return (
-        <div>AboutPage</div>
+        <div className="aboutPage">
+            <div>
+                <h1>Sobre Nosotras</h1>
+                <p>Somos un grupo de amigas que le gusta mucho pasar su tiempo creando cosas lindas.</p>
+                <p>Todo lo que verás en la pagina esta hecho 100% a mano y con mucho amor.</p>
+            </div>
+            <div className="btn-Contacto">
+            <button onClick={() => navigate('/contact')}>Quiero contactarme con ustedes!</button>
+            </div>
+        </div>
     )
 }
 
